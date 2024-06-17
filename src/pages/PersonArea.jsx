7094169d0527137d929fda0042310f4dcad3a211
Kryar <ms.zyathin@gmail.com>
2024-06-17 04:21:43 +0400

    person_page_history

--- src/pages/PersonArea.jsx
+++ src/pages/PersonArea.jsx
@@ -4,6 +4,8 @@ import '../styles/PersonArea.css';
 import Card from "../UI/svg/Card";
 import Safe from "../UI/svg/Safe";
 import Wallet from "../UI/svg/Wallet";
+import Arrow from "../UI/svg/Arrow";
+import AroundArrow from "../UI/svg/AroundArrow";
 
 const PersonArea = () => {
     return (
@@ -83,7 +85,117 @@ const PersonArea = () => {
 
                         </div>
                     </div>
-                    <div className={'right_block_person'}></div>
+                    <div className={'right_block_person'}>
+                        <div className={'trans_container'}>
+                            <div className={'check_container'}>
+                                <div className={'expense_block'}>
+                                    <div className={'text_expense'}>Расходы за июнь</div>
+                                    <div className={"strip_container"}>
+                                        <div className={'strip-one'}>200P</div>
+                                        <div className={'strip-two'}>300P</div>
+                                        <div className={'strip-three'}>800P</div>
+                                        <div className={'strip-four'}>800P</div>
+                                        <div className={'strip-five'}>100P</div>
+                                        <div className={'strip-six'}>200P</div>
+                                    </div>
+                                </div>
+                                <div className={'expense_block'}>
+                                    <div className={'text_expense'}>Зачисления за июнь</div>
+                                    <div className={"strip_container"}>
+                                        <div className={'strip-one'}>200P</div>
+                                        <div className={'strip-two'}>300P</div>
+                                        <div className={'strip-three'}>800P</div>
+                                        <div className={'strip-four'}>800P</div>
+                                        <div className={'strip-five'}>100P</div>
+                                        <div className={'strip-six'}>200P</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"filters_cont"}>
+                            <div className={"filters"}>Тип операции <div className={'Arr'}>
+                                <Arrow/></div></div>
+                            <div className={"filters"}>Период
+                                <div className={'Arr'}><Arrow/></div>
+                            </div>
+                            <div className={"filters"}>Карта или счет
+                                <div className={'Arr'}><Arrow/></div>
+                            </div>
+                            <div className={"filters"}>Сумма
+                                <div className={'Arr'}><Arrow/></div>
+                            </div>
+                        </div>
+                        <div className={'days_container'}>
+                            <div className={'day_name'}>Сегодня</div>
+                            <div className={'person_container'}>
+                                <div className={'connect'}>
+                                    <div className={'around_arrow'}><AroundArrow/></div>
+                                    <div className={'person_name_container'}>
+                                        <div className={'person_name'}>Степан Денисович Д.</div>
+                                        <div className={'operation_type'}>Входящий перевод</div>
+                                    </div>
+                                </div>
+
+                                <div className={'money_change_plus'}>+100P</div>
+                            </div>
+                            <div className={'person_container'}>
+                                <div className={'connect'}>
+                                    <div className={'around_arrow'}><AroundArrow/></div>
+                                    <div className={'person_name_container'}>
+                                        <div className={'person_name'}>Степан Денисович Д.</div>
+                                        <div className={'operation_type'}>Входящий перевод</div>
+                                    </div>
+                                </div>
+
+                                <div className={'money_change_plus'}>+100P</div>
+                            </div>
+                            <div className={'person_container'}>
+                                <div className={'connect'}>
+                                    <div className={'around_arrow'}><AroundArrow/></div>
+                                    <div className={'person_name_container'}>
+                                        <div className={'person_name'}>Степан Денисович Д.</div>
+                                        <div className={'operation_type'}>Входящий перевод</div>
+                                    </div>
+                                </div>
+
+                                <div className={'money_change_plus'}>+100P</div>
+                            </div>
+                            <div className={'day_name'}>Вчера</div>
+                            <div className={'person_container'}>
+                                <div className={'connect'}>
+                                    <div className={'around_arrow'}><AroundArrow/></div>
+                                    <div className={'person_name_container'}>
+                                        <div className={'person_name'}>Степан Денисович Д.</div>
+                                        <div className={'operation_type'}>Входящий перевод</div>
+                                    </div>
+                                </div>
+
+                                <div className={'money_change'}>100P</div>
+                            </div>
+                            <div className={'person_container'}>
+                                <div className={'connect'}>
+                                    <div className={'around_arrow'}><AroundArrow/></div>
+                                    <div className={'person_name_container'}>
+                                        <div className={'person_name'}>Степан Денисович Д.</div>
+                                        <div className={'operation_type'}>Входящий перевод</div>
+                                    </div>
+                                </div>
+
+                                <div className={'money_change'}>100P</div>
+                            </div>
+                            <div className={'person_container'}>
+                                <div className={'connect'}>
+                                    <div className={'around_arrow'}><AroundArrow/></div>
+                                    <div className={'person_name_container'}>
+                                        <div className={'person_name'}>Степан Денисович Д.</div>
+                                        <div className={'operation_type'}>Входящий перевод</div>
+                                    </div>
+                                </div>
+
+                                <div className={'money_change'}>100P</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
